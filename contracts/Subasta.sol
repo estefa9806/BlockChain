8e15354e8449e789d6f4e3f06a6666aa1b840130
estefa9806 <estefa9806@gmail.com>
2022-11-17 19:26:13 -0500

    se modifica la informacion

--- contracts/Subasta.sol
+++ contracts/Subasta.sol
@@ -1,36 +1,44 @@
 pragma solidity ^0.4.0;
 
 contract Subasta{
-
+uint256 private tiempoTotalMinToMils;
  uint256 public id=0;
  string public nombreSubasta;
  uint256 public valor;
  bool public estado;
  uint256 public tiempoCreacion;
- uint256 public tiempoLimite;
+ uint256 public tiempoLimiteEnMin;
 
-constructor(string  _nombreSubasta, uint256  _valor,uint256  _tiempoLimite) public{
+constructor(string  _nombreSubasta,uint256  _tiempoLimite) payable public{
         id++;
         nombreSubasta = _nombreSubasta;
-        valor = _valor;
+        valor = msg.value;
         estado = true;
         tiempoCreacion=block.timestamp;
-        tiempoLimite=tiempoCreacion+_tiempoLimite;
+        tiempoTotalMinToMils = _tiempoLimite*60000 ;
+        tiempoLimiteEnMin=tiempoCreacion+tiempoTotalMinToMils;
     }
    function pujar() payable public{
         require (valor > msg.value,"Dinero insuficiente"); 
-        require (!estado && tiempoLimite-block.timestamp<=0,"Subasta no disponibleS"); 
+        require (!estado,"El estado ya no es valido para la subasta"); 
+        require ( tiempoTotalMinToMils-block.timestamp<=0,"Lo sentimo el tiempo de la subasta termino"); 
+        if(msg.value>valor){
+            selfdestruct(msg.sender); //se retorna el dinero al remitente anterior
+        }
         valor = msg.value;
-       //Verificar saldo del remitente
-       address addr = msg.sender;
-       // Obtener saldo
-        uint balance = addr.balance;
-       //Obtener saldo del contrato
-       address(this).balance;
-       //Trasferir 
-      addr.transfer(valor);
-      bool success = addr.send(valor);
+        address addr = msg.sender;  //Verificar saldo del remitente
+        address(this).balance; //Obtener saldo del contrato
+        addr.transfer(valor); //Trasferir 
+        bool success = addr.send(valor);
         require (success == true,"Se transfirio el dinero"); 
+    }
+
+    function ganador()  payable public{
+        require (tiempoTotalMinToMils-block.timestamp > 0,"El tiempo aún no termina, no se puede definir quien gana"); 
+        if(tiempoTotalMinToMils-block.timestamp <= 0){
+            msg.sender;
+            msg.value;
+        }
     }
 
 
